7aea03fae49d892c571adf4d33f94fe4e7312fa2
Grzegorz Majta <grzegorz.majta@krakweb>
2019-01-28 15:54:16 +0100

    Fixes on navbar and mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,22 @@
+// Wyskakiwanie okienka Search w navbarze
+
 const btnSearch = document.getElementById('search')
 const searchWindow = document.querySelector('.search-window')
 const icon = document.querySelector('.fa-search')
 
-btnSearch.addEventListener('click',()=>{
+btnSearch.addEventListener('click',function(e){
+    e.preventDefault()
     searchWindow.classList.toggle('active')
     icon.classList.toggle('fa-search')
     icon.classList.toggle('fa-times-circle')
+})
+
+// Wyskakiwanie mobilnego Navbaru
+
+const menu = document.querySelector('.hamburger')
+const nav = document.querySelector('.nav-all')
+
+menu.addEventListener('click',function(){
+    menu.classList.toggle('active');
+    nav.classList.toggle('mobile')
 })
