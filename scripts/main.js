7b98148987a4f3a4acae8a3068efa7c9279d731a
Mateusz Swiatek <31080298+SwiatekOrg@users.noreply.github.com>
2019-01-29 23:32:15 +0100

    Duzo poprawek!

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+//menu i search ikon dzialaja tylko gdy scrolly > 150
+
 // Wyskakiwanie okienka Search w navbarze
 
 const btnSearch = document.getElementById('search')
@@ -6,9 +8,11 @@ const icon = document.querySelector('.fa-search')
 
 btnSearch.addEventListener('click',function(e){
     e.preventDefault()
-    searchWindow.classList.toggle('active')
-    icon.classList.toggle('fa-search')
-    icon.classList.toggle('fa-times-circle')
+    if(window.scrollY > 150){
+        searchWindow.classList.toggle('active')
+        icon.classList.toggle('fa-search')
+        icon.classList.toggle('fa-times-circle')
+    }
 })
 
 // Wyskakiwanie mobilnego Navbaru
@@ -17,8 +21,24 @@ const menu = document.querySelector('.hamburger')
 const nav = document.querySelector('.nav-all')
 
 menu.addEventListener('click',function(){
+    if(window.scrollY > 150){
     menu.classList.toggle('active');
     nav.classList.toggle('mobile')
+    searchWindow.classList.toggle('activeMobile')
+    }
 })
 
-// Zmniejszony navbar po zjechaniu
+// Zmniejszony navbar po zjechaniu oraz resetowanie wlaczonych
+// ficzerow przy przejsciu
+
+document.addEventListener('scroll',function(){
+    if(window.scrollY > 150) nav.classList.add('top')
+    else {
+        nav.classList.remove('top')
+        searchWindow.classList.remove('active')
+        icon.classList.add('fa-search')
+        icon.classList.remove('fa-times-circle')
+        nav.classList.remove('mobile')
+        menu.classList.remove('active')
+    }
+})
